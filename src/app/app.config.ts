cb32b6c8ac81443155a83feb64ddd42fec3ab8da
Betty <betty.spurgeon@wasdi.cloud>
2024-09-11 14:24:07 +0200

    configure translation service with test component

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,19 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { HttpClientModule, provideHttpClient } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
+
+import { provideTranslation } from './config/translate-loader.config';
+import { FormsModule } from '@angular/forms';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [provideRouter(routes), provideClientHydration(), provideHttpClient(), BrowserAnimationsModule, BrowserModule, CommonModule, FormsModule,
+  importProvidersFrom(HttpClientModule),
+  importProvidersFrom(TranslateModule.forRoot(provideTranslation()))
+  ]
 };
