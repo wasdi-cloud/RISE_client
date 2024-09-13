341ad026e6ae400d61260d87cfa6bcf2ae3e97ba
Betty <betty.spurgeon@wasdi.cloud>
2024-09-13 16:28:52 +0200

    Prelim app routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LoginViewComponent } from './views/login-view/login-view.component';
+import { PublicHomeComponent } from './views/public-home/public-home.component';
+import { DashboardComponent } from './views/dashboard/dashboard.component';
+import { AccountComponent } from './views/account/account.component';
+import { MonitorComponent } from './views/monitor/monitor.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: PublicHomeComponent },
+    { path: 'account', component: AccountComponent },
+    { path: 'dashboard', component: DashboardComponent },
+    { path: 'login', component: LoginViewComponent },
+    { path: 'monitor', component: MonitorComponent },
+];
