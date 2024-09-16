b68d327ab360339f0b8c8e8d9a597e0974d58216
Betty <betty.spurgeon@wasdi.cloud>
2024-09-16 09:55:53 +0200

    Sign up view and update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
-import { LoginViewComponent } from './views/login-view/login-view.component';
-import { PublicHomeComponent } from './views/public-home/public-home.component';
-import { DashboardComponent } from './views/dashboard/dashboard.component';
+
 import { AccountComponent } from './views/account/account.component';
+import { DashboardComponent } from './views/dashboard/dashboard.component';
+import { LoginViewComponent } from './views/login-view/login-view.component';
 import { MonitorComponent } from './views/monitor/monitor.component';
+import { PublicHomeComponent } from './views/public-home/public-home.component';
+import { SignUpComponent } from './views/sign-up/sign-up.component';
 
 export const routes: Routes = [
     { path: '', component: PublicHomeComponent },
@@ -11,4 +13,5 @@ export const routes: Routes = [
     { path: 'dashboard', component: DashboardComponent },
     { path: 'login', component: LoginViewComponent },
     { path: 'monitor', component: MonitorComponent },
+    { path: 'sign-up', component: SignUpComponent }
 ];
