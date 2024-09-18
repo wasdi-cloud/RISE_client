379db534e968ecd7ff4034f5ade46eb04778e016
Betty <betty.spurgeon@wasdi.cloud>
2024-09-18 17:09:03 +0200

    Monitor and public-home pages with toolbar

--- src/app/views/monitor/monitor.component.ts
+++ src/app/views/monitor/monitor.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RiseTimebarComponent } from '../../components/rise-timebar/rise-timebar.component';
+import { RiseMapComponent } from '../../components/rise-map/rise-map.component';
+import { RiseToolbarComponent } from '../../components/rise-toolbar/rise-toolbar.component';
 
 @Component({
   selector: 'app-monitor',
   standalone: true,
-  imports: [],
+  imports: [RiseMapComponent, RiseToolbarComponent, RiseTimebarComponent],
   templateUrl: './monitor.component.html',
   styleUrl: './monitor.component.css'
 })
--- src/app/views/public-home/public-home.component.ts
+++ src/app/views/public-home/public-home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RiseToolbarComponent } from '../../components/rise-toolbar/rise-toolbar.component';
 
 @Component({
   selector: 'app-public-home',
   standalone: true,
-  imports: [],
+  imports: [RiseToolbarComponent],
   templateUrl: './public-home.component.html',
   styleUrl: './public-home.component.css'
 })
