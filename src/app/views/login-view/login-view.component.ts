f4c8f4f19c7ff979ba6ad9e4cd1a8861166bcf65
Betty <betty.spurgeon@wasdi.cloud>
2024-09-24 15:25:01 +0200

    Login component

--- src/app/views/login-view/login-view.component.ts
+++ src/app/views/login-view/login-view.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RiseTextInputComponent } from '../../components/rise-text-input/rise-text-input.component';
+import { AuthService } from '../../services/api/auth.service';
+import { RiseButtonComponent } from '../../components/rise-button/rise-button.component';
+import { UserCredentials } from '../../shared/models/user-credentials';
+import { OtpDialogComponent } from '../../dialogs/otp-dialog/otp-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
+
 
 @Component({
   selector: 'app-login-view',
   standalone: true,
-  imports: [],
+  imports: [RiseButtonComponent, RiseTextInputComponent, OtpDialogComponent,],
   templateUrl: './login-view.component.html',
   styleUrl: './login-view.component.css'
 })
 export class LoginViewComponent {
+  public m_oUserInput: UserCredentials = {
+    userId: "",
+    password: ""
+  };
 
+  constructor(private m_oAuthService: AuthService, private m_oDialog: MatDialog) { }
+
+  executeLogin() {
+    // this.m_oAuthService.loginUser(this.m_oUserInput); -> Returns OTP View Model
+
+    let oOtpViewModel;
+    this.m_oDialog.open(OtpDialogComponent, {
+      data: oOtpViewModel
+    });
+  }
 }
