10814be6192a29d293a8eab3a84e8a35028e5927
Betty <betty.spurgeon@wasdi.cloud>
2024-09-25 11:21:38 +0200

    account -> dashboard -> monitor flow

--- src/app/views/account/account.component.ts
+++ src/app/views/account/account.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RiseToolbarComponent } from '../../components/rise-toolbar/rise-toolbar.component';
 
 @Component({
   selector: 'app-account',
   standalone: true,
-  imports: [],
+  imports: [RiseToolbarComponent],
   templateUrl: './account.component.html',
   styleUrl: './account.component.css'
 })
--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { RiseToolbarComponent } from '../../components/rise-toolbar/rise-toolbar.component';
+import { RiseButtonComponent } from '../../components/rise-button/rise-button.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [RiseToolbarComponent, RiseButtonComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+  constructor(private m_oRouter: Router) { }
 
+  public navigateRoute(sRoute: string): void {
+    this.m_oRouter.navigateByUrl(sRoute)
+  }
 }
