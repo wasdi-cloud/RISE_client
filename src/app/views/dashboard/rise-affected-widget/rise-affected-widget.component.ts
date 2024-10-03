f53cde679bb34028b2d4cec49123c5714b7b4998
Betty <betty.spurgeon@wasdi.cloud>
2024-10-03 14:06:13 +0200

    [ADDED] Affected People Widget

---
+++ src/app/views/dashboard/rise-affected-widget/rise-affected-widget.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'rise-affected-widget',
+  standalone: true,
+  imports: [],
+  templateUrl: './rise-affected-widget.component.html',
+  styleUrl: './rise-affected-widget.component.css'
+})
+export class RiseAffectedWidgetComponent {
+
+}
