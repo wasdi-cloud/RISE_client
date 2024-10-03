3d41f5650378517add0de9838065022e9271341f
Betty <betty.spurgeon@wasdi.cloud>
2024-10-03 11:50:39 +0200

    [ADDED] Layer types.ts file

---
+++ src/app/views/monitor/layer-types.ts
@@ -0,0 +1,18 @@
+export const LayerTypes = [
+    {
+        translateKey: "LAYER_TYPES.FLOOD",
+        selection: 'flood'
+    },
+    {
+        translateKey: "LAYER_TYPES.DROUGHT",
+        selection: 'drought'
+    },
+    {
+        translateKey: "LAYER_TYPES.BUILDINGS",
+        selection: 'buildings'
+    },
+    {
+        translateKey: "LAYER_TYPES.IMPACTS",
+        selection: 'impacts'
+    }
+]
