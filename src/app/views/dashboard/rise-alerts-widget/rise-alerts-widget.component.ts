4f1a4a72731caff6975256add60519558a8e22e2
Betty <betty.spurgeon@wasdi.cloud>
2024-10-04 15:12:12 +0200

    Complete ongoing events + alerts widgets

--- src/app/views/dashboard/rise-alerts-widget/rise-alerts-widget.component.ts
+++ src/app/views/dashboard/rise-alerts-widget/rise-alerts-widget.component.ts
@@ -1,12 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'rise-alerts-widget',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './rise-alerts-widget.component.html',
   styleUrl: './rise-alerts-widget.component.css'
 })
 export class RiseAlertsWidgetComponent {
- @Input() m_aoAlerts: Array<any> = [];
+  @Input() m_aoAlerts: Array<any> = [];
+
+  m_bShowContent: boolean = true;
+
+  collapseWidget() {
+    this.m_bShowContent = !this.m_bShowContent;
+  }
 }
--- src/app/views/dashboard/rise-ongoing-widget/rise-ongoing-widget.component.ts
+++ src/app/views/dashboard/rise-ongoing-widget/rise-ongoing-widget.component.ts
@@ -1,13 +1,21 @@
 import { Component, Input } from '@angular/core';
 import { Event } from '../../../shared/models/event';
+import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'rise-ongoing-widget',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './rise-ongoing-widget.component.html',
   styleUrl: './rise-ongoing-widget.component.css'
 })
 export class RiseOngoingWidgetComponent {
-  @Input() m_aoOngoingEvent: Array<Event> =[];
+  @Input() m_aoOngoingEvent: Array<Event> = [];
+
+  m_bShowContent: boolean = true;
+
+  collapseWidget() {
+    this.m_bShowContent = !this.m_bShowContent;
+  }
 }
