279d7ce8b9ea427054a226b82eec760334fb3e69
Betty <betty.spurgeon@wasdi.cloud>
2024-10-14 09:54:49 +0200

    [FINISHED] selection for outlet

--- src/app/views/account/account-sidebar/account-sidebar.component.ts
+++ src/app/views/account/account-sidebar/account-sidebar.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RiseButtonComponent } from '../../../components/rise-button/rise-button.component';
 
 @Component({
   selector: 'account-sidebar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RiseButtonComponent],
   templateUrl: './account-sidebar.component.html',
   styleUrl: './account-sidebar.component.css',
 })
 export class AccountSidebarComponent {
+  @Input() m_aoAccountButtons = [];
+
+  @Output() m_sSelectedButton: EventEmitter<any> = new EventEmitter<any>(null);
+
   constructor() {}
+
+  emitButtonSelection(sSelectedBtn: string) {
+    this.m_sSelectedButton.emit(sSelectedBtn);
+  }
 }
--- src/app/views/account/account.component.ts
+++ src/app/views/account/account.component.ts
@@ -1,24 +1,37 @@
 import { Component } from '@angular/core';
 import { RiseToolbarComponent } from '../../components/rise-toolbar/rise-toolbar.component';
-import {RiseButtonComponent} from "../../components/rise-button/rise-button.component";
-import {TranslateModule} from "@ngx-translate/core";
-import {Router} from "@angular/router";
-
+import { RiseButtonComponent } from '../../components/rise-button/rise-button.component';
+import { TranslateModule } from '@ngx-translate/core';
+import { Router } from '@angular/router';
+import { AccountSidebarComponent } from './account-sidebar/account-sidebar.component';
+import { AccountBtns } from './account-sidebar/account-btns';
 @Component({
   selector: 'app-account',
   standalone: true,
-    imports: [RiseToolbarComponent, RiseButtonComponent, TranslateModule],
+  imports: [
+    AccountSidebarComponent,
+    RiseToolbarComponent,
+    RiseButtonComponent,
+    TranslateModule,
+  ],
   templateUrl: './account.component.html',
-  styleUrl: './account.component.css'
+  styleUrl: './account.component.css',
 })
 export class AccountComponent {
+  m_sActiveOutlet: string = 'user';
 
-  constructor(
-    private m_oRouter:Router
-  ) {
-  }
+  m_aoAccountButtons: Array<any> = AccountBtns;
+
+  constructor(private m_oRouter: Router) {}
 
   public navigateRoute(sLocation: string) {
     this.m_oRouter.navigateByUrl(`/${sLocation}`);
+  }
+
+  public getActiveOutlet(sSelectedOutlet: string) {
+    if (sSelectedOutlet) {
+      this.m_sActiveOutlet = sSelectedOutlet;
+      console.log(this.m_sActiveOutlet)
+    }
   }
 }
