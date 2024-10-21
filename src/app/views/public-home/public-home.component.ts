a9db3aec6950e9bc1c796dd275ad9febe54b4022
Betty <betty.spurgeon@wasdi.cloud>
2024-10-21 15:41:06 +0200

    prelim styling for homepage

--- src/app/views/public-home/public-home.component.ts
+++ src/app/views/public-home/public-home.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RiseToolbarComponent } from '../../components/rise-toolbar/rise-toolbar.component';
+import { RiseButtonComponent } from '../../components/rise-button/rise-button.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-public-home',
   standalone: true,
-  imports: [RiseToolbarComponent],
+  imports: [RiseButtonComponent, RiseToolbarComponent],
   templateUrl: './public-home.component.html',
-  styleUrl: './public-home.component.css'
+  styleUrl: './public-home.component.css',
 })
 export class PublicHomeComponent {
+  constructor(private m_oRouter: Router) {}
 
+  navigateRoute(sRouter: string) {
+    this.m_oRouter.navigateByUrl(sRouter);
+  }
 }
