d96a8abf074319a5fc21965ce28ecf5e78f6f30a
Betty <betty.spurgeon@wasdi.cloud>
2024-10-21 14:19:49 +0200

    Rise button prelim styling

--- src/app/components/rise-button/rise-button.component.ts
+++ src/app/components/rise-button/rise-button.component.ts
@@ -1,38 +1,39 @@
+import { NgClass } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'rise-button',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [NgClass, TranslateModule],
   templateUrl: './rise-button.component.html',
-  styleUrl: './rise-button.component.css'
+  styleUrl: './rise-button.component.css',
 })
 export class RiseButtonComponent {
   /**
    * The label that will appear on interior of the button
    */
-  @Input() m_sLabel: string = "default";
-
-  /**
-  * The size of the button: small or large. Medium by default
-  */
-  @Input() m_sSize?: 'small' | 'medium' | 'large' = "medium";
+  @Input() m_sLabel: string = 'default';
 
   /**
    * What is the role of the button? (Colour)
    */
-  @Input() m_sRole?: 'success' | 'danger' | 'warning' | 'default' = "default"
+  @Input() m_sRole?:
+    | 'action'
+    | 'danger'
+    | 'muted'
+    | 'primary'
+    | 'regular' = 'regular';
 
   /**
    * The Icon that will appear on the left side of the button
    */
-  @Input() m_sIconLeft: string = "";
+  @Input() m_sIconLeft: string = '';
 
   /**
    * The Icon that will appear on the right side of the button
    */
-  @Input() m_sIconRight: string = "";
+  @Input() m_sIconRight: string = '';
 
   /**
    * Is the button disabled?
@@ -40,17 +41,17 @@ export class RiseButtonComponent {
   @Input() m_bDisabled?: boolean = false;
 
   /**
-   * Is the button a link? 
+   * Is the button a link?
    */
   @Input() m_bLink?: boolean = false;
 
   /**
-   * Is the button rounded? 
+   * Is the button rounded?
    */
   @Input() m_bRounded?: boolean = false;
 
   /**
-   * Is the button outlined instead of solid? 
+   * Is the button outlined instead of solid?
    */
   @Input() m_bOutline?: boolean = false;
 
