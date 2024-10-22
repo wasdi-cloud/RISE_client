0d52f01976cb1f1e689744157d9d494980839aa4
Betty <betty.spurgeon@wasdi.cloud>
2024-10-22 09:59:25 +0200

    input field styling

--- src/app/components/rise-text-input/rise-text-input.component.ts
+++ src/app/components/rise-text-input/rise-text-input.component.ts
@@ -3,18 +3,17 @@ import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
 import { FormsModule } from '@angular/forms';
 
-
 @Component({
   selector: 'rise-text-input',
   standalone: true,
   imports: [CommonModule, FormsModule, TranslateModule],
   templateUrl: './rise-text-input.component.html',
-  styleUrl: './rise-text-input.component.css'
+  styleUrl: './rise-text-input.component.css',
 })
 export class RiseTextInputComponent {
   @Input() m_sName: string;
-  @Input() m_sLocalizationKey: string = "TEST";
-  @Input() m_sType: 'text' | 'password' = "text"
+  @Input() m_sLocalizationKey: string = 'TEST';
+  @Input() m_sType: 'text' | 'password' = 'text';
 
   /**
    * Is this input part of a validator (i.e., checking two inputs are the same)
@@ -27,7 +26,13 @@ export class RiseTextInputComponent {
   @Input() m_bIsValid: boolean = true;
 
   @Input() m_sInputText: string;
-  @Output() m_sInputTextChange: EventEmitter<string> = new EventEmitter<string>();
+
+  @Input() m_bHasIcon: boolean = false;
+
+  @Input() m_sIcon: string = 'search';
+
+  @Output() m_sInputTextChange: EventEmitter<string> =
+    new EventEmitter<string>();
 
   onInputChange() {
     this.m_sInputTextChange.emit(this.m_sInputText);
