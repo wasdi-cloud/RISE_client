527df8a20363cd6930ea0456148f6438b340cabc
jihed-Sh <jihedcr7@gmail.com>
2024-10-23 13:59:07 +0100

    [WIP] error for translating

--- src/app/services/interceptors/server-error.interceptor.ts
+++ src/app/services/interceptors/server-error.interceptor.ts
@@ -8,7 +8,7 @@ import {TranslateService} from '@ngx-translate/core';
 export const serverErrorInterceptor: HttpInterceptorFn = (req, next) => {
 
   const m_oNotificationService = inject(NotificationsDialogsService);
-  const m_oTranslate = inject(TranslateService);
+  // const m_oTranslate = inject(TranslateService);
 
   return next(req).pipe(
     tap((httpEvent) => {
@@ -18,9 +18,11 @@ export const serverErrorInterceptor: HttpInterceptorFn = (req, next) => {
       let sErrorMessage = 'An unexpected error occurred.';
       //TODO: Client-side or network error
       //TODO: Check if it's a server-side error
+
+
       if (error.error.errorStringCodes[0]) {
-        let translatedMessage = m_oTranslate.instant(error.error.errorStringCodes[0]);
-        sErrorMessage = `Error: ${translatedMessage}`;
+        // let translatedMessage = m_oTranslate.instant(error.error.errorStringCodes[0]);
+        sErrorMessage = `Error: ${error.error.errorStringCodes[0]}`;
       }
       m_oNotificationService.openInfoDialog(
         sErrorMessage, 'danger', "Error"
