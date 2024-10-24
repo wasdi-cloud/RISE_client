f26dd76ba112e9d61c6e82bbdd731ee0325005b1
Betty <betty.spurgeon@wasdi.cloud>
2024-10-24 11:33:51 +0200

    Textarea styling

--- src/app/components/rise-textarea-input/rise-textarea-input.component.ts
+++ src/app/components/rise-textarea-input/rise-textarea-input.component.ts
@@ -3,19 +3,41 @@ import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
 import { FormsModule } from '@angular/forms';
 
-
 @Component({
   selector: 'rise-textarea-input',
   standalone: true,
   imports: [CommonModule, FormsModule, TranslateModule],
   templateUrl: './rise-textarea-input.component.html',
-  styleUrl: './rise-textarea-input.component.css'
+  styleUrl: './rise-textarea-input.component.css',
 })
 export class RiseTextareaInputComponent {
+  /**
+   * Name field for textarea
+   */
   @Input() m_sName: string;
-  @Input() m_sLocalizationKey: string = "TEST";
+
+  /**
+   * Localization key for corresponding translation in i18n
+   */
+  @Input() m_sLocalizationKey: string = '';
+
+  /**
+   * Input text model
+   */
   @Input() m_sInputText: string;
-  @Output() m_sInputTextChange: EventEmitter<string> = new EventEmitter<string>();
+
+  /**
+   * Is this input part of a validator (i.e., checking two inputs are the same)
+   */
+  @Input() m_bIsValidatorInput?: boolean = false;
+
+  /**
+   * Is the input valid?
+   */
+  @Input() m_bIsValid: boolean = true;
+
+  @Output() m_sInputTextChange: EventEmitter<string> =
+    new EventEmitter<string>();
 
   onInputChange() {
     this.m_sInputTextChange.emit(this.m_sInputText);
