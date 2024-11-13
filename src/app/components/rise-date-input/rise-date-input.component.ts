d62fcfded28414886c6bf2baabd38bbe0fc2a14d
Betty <betty.spurgeon@wasdi.cloud>
2024-11-13 10:29:49 +0100

    [ADDED] functionality for date inputs

--- src/app/components/rise-date-input/rise-date-input.component.ts
+++ src/app/components/rise-date-input/rise-date-input.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'rise-date-input',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, TranslateModule],
   templateUrl: './rise-date-input.component.html',
-  styleUrl: './rise-date-input.component.css'
+  styleUrl: './rise-date-input.component.css',
 })
 export class RiseDateInputComponent {
+  @Input() m_bHasTitle?: boolean = false;
 
+  @Input() m_sName: string = '';
+
+  @Input() m_sLocalizationKey: string = '';
+
+  @Input() m_bReadonly: boolean = false;
+
+  @Input() m_bRequired: boolean = false;
+
+  @Input() m_sInputDate: any = null;
+
+  @Output() m_sInputDateChange: EventEmitter<any> = new EventEmitter<any>();
+
+  onInputChange() {}
 }
