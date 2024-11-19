ff99e29edd9d3c752f7a6be5a46c98dbd81c0dfd
Betty <betty.spurgeon@wasdi.cloud>
2024-11-19 17:00:10 +0100

    [FINISHED] widget styling w/test

--- src/app/views/dashboard/rise-ongoing-widget/rise-ongoing-widget.component.ts
+++ src/app/views/dashboard/rise-ongoing-widget/rise-ongoing-widget.component.ts
@@ -1,19 +1,34 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { EventViewModel } from '../../../models/EventViewModel';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
+import { RiseBadgeComponent } from '../../../components/rise-badge/rise-badge.component';
 
 @Component({
   selector: 'rise-ongoing-widget',
   standalone: true,
-  imports: [CommonModule, TranslateModule],
+  imports: [CommonModule, TranslateModule, RiseBadgeComponent],
   templateUrl: './rise-ongoing-widget.component.html',
   styleUrl: './rise-ongoing-widget.component.css',
 })
-export class RiseOngoingWidgetComponent {
+export class RiseOngoingWidgetComponent implements OnInit {
   @Input() m_aoOngoingEvent: Array<EventViewModel> = [];
 
+  testEvent: EventViewModel = {
+    name: 'Madagascar',
+    id: '',
+    startDate: Date.now(),
+    endDate: 0,
+    type: 'Flood',
+    bbox: '',
+    peakDate: 0,
+  };
+
   m_bShowContent: boolean = true;
+
+  ngOnInit(): void {
+    // this.m_aoOngoingEvent.push(this.testEvent);
+  }
 
   collapseWidget() {
     this.m_bShowContent = !this.m_bShowContent;
