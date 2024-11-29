499d0f5ebf0678de33c83c960387e1ad7f783c80
Betty <betty.spurgeon@wasdi.cloud>
2024-11-29 16:35:51 +0100

    Area of ops

--- src/app/views/area-of-operations/area-of-operations.component.ts
+++ src/app/views/area-of-operations/area-of-operations.component.ts
@@ -1,24 +1,23 @@
-import {Component, OnInit} from '@angular/core';
-import {TranslateModule, TranslateService} from '@ngx-translate/core';
+import { Component, OnInit } from '@angular/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
-import {RiseButtonComponent} from '../../components/rise-button/rise-button.component';
+import { RiseButtonComponent } from '../../components/rise-button/rise-button.component';
 
-import {AreaViewModel} from '../../models/AreaViewModel';
-import {AreaService} from '../../services/api/area.service';
+import { AreaViewModel } from '../../models/AreaViewModel';
+import { AreaService } from '../../services/api/area.service';
 
-import {RiseMapComponent} from '../../components/rise-map/rise-map.component';
-import {NgFor, NgIf} from '@angular/common';
+import { RiseMapComponent } from '../../components/rise-map/rise-map.component';
+import { NgFor, NgIf } from '@angular/common';
 
-import {MatDialog} from '@angular/material/dialog';
-import {CreateAreaOfOperationComponent} from '../create-area-of-operation/create-area-of-operation.component';
+import { MatDialog } from '@angular/material/dialog';
+import { CreateAreaOfOperationComponent } from '../create-area-of-operation/create-area-of-operation.component';
 
-import {MapService} from '../../services/map.service';
+import { MapService } from '../../services/map.service';
 
-import {NotificationsDialogsService} from '../../services/notifications-dialogs.service';
-import {
-  RiseCollaboratorsDialogComponent
-} from '../../components/rise-collaborators-dialog/rise-collaborators-dialog.component';
-import {AreaInfoComponent} from './area-info/area-info.component';
+import { NotificationsDialogsService } from '../../services/notifications-dialogs.service';
+import { RiseCollaboratorsDialogComponent } from '../../components/rise-collaborators-dialog/rise-collaborators-dialog.component';
+import { AreaInfoComponent } from './area-info/area-info.component';
+import FadeoutUtils from '../../shared/utilities/FadeoutUtils';
 
 @Component({
   selector: 'app-area-of-operations',
@@ -44,8 +43,7 @@ export class AreaOfOperationsComponent implements OnInit {
     private m_oMapService: MapService,
     private m_oNotificationService: NotificationsDialogsService,
     private m_oTranslate: TranslateService
-  ) {
-  }
+  ) {}
 
   ngOnInit() {
     this.getAreas();
@@ -63,7 +61,7 @@ export class AreaOfOperationsComponent implements OnInit {
   toggleShowNew(bShowNew: boolean) {
     this.m_bShowNewArea = bShowNew;
     if (!this.m_bShowNewArea) {
-      this.getAreas()
+      this.getAreas();
     }
   }
 
@@ -71,82 +69,82 @@ export class AreaOfOperationsComponent implements OnInit {
     this.m_oMapService.flyToMonitorBounds(oArea.bbox);
   }
 
-  openManageUsers(oArea) {
-    console.log('managing users');
+  openManageUsers(oArea: AreaViewModel): void {
+    let sErrorMsg: string = this.m_oTranslate.instant(
+      'AREA_OF_OPERATIONS.ERROR_GET_USERS'
+    );
     this.m_oAreaService.getUsersFromArea(oArea.id).subscribe({
       next: (oResponse) => {
-        console.log(oResponse);
+        if (FadeoutUtils.utilsIsObjectNullOrUndefined(oResponse)) {
+          return;
+        }
+        this.m_oDialog.open(RiseCollaboratorsDialogComponent, {
+          data: {
+            users: oResponse,
+            resourceType: 'area',
+            id: oArea.id,
+            isDialog: true,
+          },
+        });
       },
       error: (oError) => {
-        // this.m_oNotificationService.openInfoDialog(
-        //   'Could not get Area Users',
-        //   'danger',
-        //   'Error'
-        // );
-      },
-    });
-    this.m_oDialog.open(RiseCollaboratorsDialogComponent, {
-      maxHeight: '500px',
-      maxWidth: '700px',
-      data: {
-        users: [],
-        isDialog: true,
+        this.m_oNotificationService.openInfoDialog(sErrorMsg, 'danger');
       },
     });
   }
 
   openEditArea(oArea) {
-    this.m_oDialog.open(AreaInfoComponent, {
-      data: {
-        area: oArea
-      }
-    }).afterClosed()
+    this.m_oDialog
+      .open(AreaInfoComponent, {
+        data: {
+          area: oArea,
+        },
+      })
+      .afterClosed()
       .subscribe((oResponse) => {
         this.getAreas();
-      })
+      });
   }
 
   deleteArea(oArea) {
     //ask user if he really wants ot delete it or update it
-    let sCheckWithUser: string = this.m_oTranslate.instant(
+    let sConfirmMsg: string = this.m_oTranslate.instant(
       'AREA_OF_OPERATIONS.DELETE_OR_UPDATE'
     );
-    sCheckWithUser += `<ul><li>${oArea.name}</li></ul>`;
-    this.m_oNotificationService.openConfirmationDialog(sCheckWithUser, 'danger').subscribe(
-      (bResult) => {
+    sConfirmMsg += `<ul><li>${oArea.name}</li></ul>`;
+    this.m_oNotificationService
+      .openConfirmationDialog(sConfirmMsg, 'danger')
+      .subscribe((bResult) => {
         if (bResult) {
-          //user wants to delete , we ask for confirmation
           this.confirmAreaDelete(oArea);
         } else {
           this.openEditArea(oArea);
         }
-      }
-    )
-
+      });
   }
 
   private confirmAreaDelete(oArea) {
     let sConfirm: string = this.m_oTranslate.instant(
       'AREA_OF_OPERATIONS.CONFIRM_DELETE'
     );
+    let sErrorMsg: string = this.m_oTranslate.instant(
+      'AREA_OF_OPERATIONS.ERROR_DELETE'
+    );
     sConfirm += `<ul><li>${oArea.name}</li></ul>`;
     this.m_oNotificationService
       .openConfirmationDialog(sConfirm, 'danger')
       .subscribe((bResult) => {
-        if (bResult) {
-          this.m_oAreaService.deleteAreaOfOperation(oArea.id).subscribe(
-            {
-              next: (oResponse) => {
-                this.getAreas();
-              }, error: (oError) => {
-                console.error(oError)
-              }
-            }
-          )
-
-        } else {
-          //do nothing (here for readability)
+        if (!bResult) {
+          return;
         }
+        this.m_oAreaService.deleteAreaOfOperation(oArea.id).subscribe({
+          next: (oResponse) => {
+            this.getAreas();
+          },
+          error: (oError) => {
+            this.m_oNotificationService.openInfoDialog(sErrorMsg, 'danger');
+          },
+        });
       });
   }
 }
