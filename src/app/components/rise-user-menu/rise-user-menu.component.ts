eeab5e0cf4569d24d8977fedea4ab8bc039c3578
Betty <betty.spurgeon@wasdi.cloud>
2024-12-03 14:37:40 +0100

    [FINISHED] clean code + error handling in RISE USER MENU

--- src/app/components/rise-user-menu/rise-user-menu.component.ts
+++ src/app/components/rise-user-menu/rise-user-menu.component.ts
@@ -1,14 +1,22 @@
-import {Component, OnInit} from '@angular/core';
-import {DefaultMenuItems, FullMenuItems, ReducedMenuItems,} from './menu-items';
-import {NgFor, NgIf} from '@angular/common';
-import {TranslateModule} from '@ngx-translate/core';
-import {ActivatedRoute, NavigationExtras, Router} from '@angular/router';
-import {MapService} from '../../services/map.service';
-import {AuthService} from '../../services/api/auth.service';
-import {UserService} from '../../services/api/user.service';
-import {UserViewModel} from '../../models/UserViewModel';
-import {ConstantsService} from '../../services/constants.service';
-import {UserRole} from "../../models/UserRole";
+import { Component, OnInit } from '@angular/core';
+import { NgFor, NgIf } from '@angular/common';
+import { ActivatedRoute, NavigationExtras, Router } from '@angular/router';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+
+import { AuthService } from '../../services/api/auth.service';
+import { ConstantsService } from '../../services/constants.service';
+import { MapService } from '../../services/map.service';
+import { NotificationsDialogsService } from '../../services/notifications-dialogs.service';
+import { UserService } from '../../services/api/user.service';
+
+import { UserRole } from '../../models/UserRole';
+import { UserViewModel } from '../../models/UserViewModel';
+
+import {
+  DefaultMenuItems,
+  FullMenuItems,
+  ReducedMenuItems,
+} from './menu-items';
 
 @Component({
   selector: 'rise-user-menu',
@@ -28,7 +36,9 @@ export class RiseUserMenuComponent implements OnInit {
     private m_oAuthService: AuthService,
     private m_oConstantsService: ConstantsService,
     private m_oMapService: MapService,
+    private m_oNotificationService: NotificationsDialogsService,
     private m_oRouter: Router,
+    private m_oTranslate: TranslateService,
     private m_oUserService: UserService
   ) {
     this.m_aoMenuItems = DefaultMenuItems;
@@ -39,11 +49,98 @@ export class RiseUserMenuComponent implements OnInit {
     this.getUserMenu();
   }
 
-  handleClick(sName) {
+  /**
+   * Initialize the user's information
+   */
+  private initUserMenu(): void {
+    let sError: string = this.m_oTranslate.instant('USER_MENU.ERROR');
+    if (!this.m_oUser?.role) {
+      this.m_oUserService.getUser().subscribe({
+        next: (oResponse) => {
+          this.m_oUser = oResponse;
+          this.m_oConstantsService.setUser(this.m_oUser);
+          if (this.m_oUser.role == UserRole.FIELD) {
+            this.m_aoMenuItems = DefaultMenuItems.filter(
+              (item) => item.name != 'subscriptions'
+            );
+          } else {
+            this.m_aoMenuItems = DefaultMenuItems;
+          }
+        },
+        error: (oError) => {
+          this.m_oNotificationService.openInfoDialog(sError, 'danger');
+        },
+      });
+    }
+  }
+
+  /**
+   * Retrieve the appropriate menu item set based on user's location and Role
+   */
+  private getUserMenu(): void {
+    let sError = this.m_oTranslate.instant('USER_MENU.ERROR');
+    this.m_oUser = this.m_oConstantsService.getUser();
+
+    if (!this.m_oUser) {
+      this.m_oUserService.getUser().subscribe({
+        next: (oResponse) => {
+          this.m_oUser = oResponse;
+          this.m_oConstantsService.setUser(this.m_oUser);
+        },
+        error: (oError) => {
+          this.m_oNotificationService.openInfoDialog(sError, 'danger');
+        },
+      });
+    }
+
+    this.m_oActivatedRoute.url.subscribe((params) => {
+      if (params.toString().includes('account')) {
+        this.m_aoMenuItems = ReducedMenuItems;
+      } else if (params.toString().includes('monitor')) {
+        let oUserRole = this.m_oConstantsService.getUserRole();
+        if (!oUserRole) {
+          this.m_oUserService.getUser().subscribe({
+            next: (oResponse) => {
+              oUserRole = oResponse.role;
+            },
+          });
+        }
+        if (oUserRole == UserRole.FIELD) {
+          this.m_aoMenuItems = FullMenuItems.filter(
+            (item) => item.name != 'subscriptions'
+          );
+        } else {
+          this.m_aoMenuItems = FullMenuItems;
+        }
+      } else {
+        let oUserRole = this.m_oConstantsService.getUserRole();
+        if (!oUserRole) {
+          this.m_oUserService.getUser().subscribe({
+            next: (oResponse) => {
+              oUserRole = oResponse.role;
+            },
+          });
+        }
+        if (oUserRole == UserRole.FIELD) {
+          this.m_aoMenuItems = DefaultMenuItems.filter(
+            (item) => item.name != 'subscriptions'
+          );
+        } else {
+          this.m_aoMenuItems = DefaultMenuItems;
+        }
+      }
+    });
+  }
+
+  /**
+   * Click handler for user inputs in dropdown state
+   * @param sName
+   */
+  handleClick(sName): void {
     switch (sName) {
       case 'subscriptions':
         let oNavExtra: NavigationExtras = {
-          state: {m_sActiveOutlet: sName},
+          state: { m_sActiveOutlet: sName },
         };
         this.m_oRouter.navigate(['account'], oNavExtra);
         break;
@@ -52,8 +149,6 @@ export class RiseUserMenuComponent implements OnInit {
         break;
       case 'logout':
         this.m_oAuthService.logout();
-        // TODO: Execute actual logout here
-
         break;
       case 'dashboard':
         this.m_oMapService.closeWorkspace();
@@ -64,82 +159,10 @@ export class RiseUserMenuComponent implements OnInit {
     }
   }
 
-  toggleDropdown() {
+  /**
+   * Toggle Dropdown visibility state
+   */
+  toggleDropdown(): void {
     this.m_bShowDropdown = !this.m_bShowDropdown;
-  }
-
-  private initUserMenu() {
-    if (!this.m_oUser?.role) {
-      this.m_oUserService.getUser().subscribe({
-        next: (oResponse) => {
-          this.m_oUser = oResponse;
-          this.m_oConstantsService.setUser(this.m_oUser);
-          if (this.m_oUser.role == UserRole.FIELD) {
-            this.m_aoMenuItems = DefaultMenuItems.filter(item => item.name != 'subscriptions');
-
-          } else {
-            this.m_aoMenuItems = DefaultMenuItems;
-          }
-
-        },
-        error: (oError) => {
-          console.error(oError);
-        },
-      });
-    }
-
-  }
-
-  private getUserMenu() {
-    this.m_oUser = this.m_oConstantsService.getUser();
-
-    if (!this.m_oUser) {
-      this.m_oUserService.getUser().subscribe({
-        next: (oResponse) => {
-          this.m_oUser = oResponse;
-          this.m_oConstantsService.setUser(this.m_oUser);
-        },
-        error: (oError) => {
-          console.error(oError);
-        },
-      });
-    }
-
-    this.m_oActivatedRoute.url.subscribe((params) => {
-      if (params.toString().includes('account')) {
-        this.m_aoMenuItems = ReducedMenuItems
-      } else if (params.toString().includes('monitor')) {
-        let oUserRole = this.m_oConstantsService.getUserRole();
-        if (!oUserRole) {
-          this.m_oUserService.getUser().subscribe({
-            next: (oResponse) => {
-              oUserRole = oResponse.role;
-            }
-          })
-        }
-        if (oUserRole == UserRole.FIELD) {
-          this.m_aoMenuItems = FullMenuItems.filter(item => item.name != 'subscriptions');
-        } else {
-          this.m_aoMenuItems = FullMenuItems
-        }
-
-      } else {
-        let oUserRole = this.m_oConstantsService.getUserRole();
-        if (!oUserRole) {
-          this.m_oUserService.getUser().subscribe({
-            next: (oResponse) => {
-              oUserRole = oResponse.role;
-            }
-          })
-        }
-        if (oUserRole == UserRole.FIELD) {
-          this.m_aoMenuItems = DefaultMenuItems.filter(item => item.name != 'subscriptions');
-
-        } else {
-          this.m_aoMenuItems = DefaultMenuItems;
-        }
-
-      }
-    });
   }
 }
