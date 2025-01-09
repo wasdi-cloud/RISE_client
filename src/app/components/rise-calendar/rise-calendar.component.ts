02df8491fc3be0d7341e659f95de97d0522dbe3d
jihed-Sh <jihedcr7@gmail.com>
2025-01-09 15:35:15 +0100

    [ADDED] date format display

--- src/app/components/rise-calendar/rise-calendar.component.ts
+++ src/app/components/rise-calendar/rise-calendar.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 import {MatFormField, MatFormFieldModule} from "@angular/material/form-field";
 import {MatInput, MatInputModule} from "@angular/material/input";
 import {
@@ -7,9 +7,21 @@ import {
   MatDatepickerModule,
   MatDatepickerToggle
 } from "@angular/material/datepicker";
-import {MatNativeDateModule, provideNativeDateAdapter} from "@angular/material/core";
+import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE, MatNativeDateModule} from "@angular/material/core";
 import {MatIcon} from "@angular/material/icon";
+import {FormsModule} from "@angular/forms";
+import {DatePipe} from "@angular/common";
+import {MomentDateAdapter} from "@angular/material-moment-adapter";
 
+export const CUSTOM_DATE_FORMATS = {
+  parse: { dateInput: 'ddd MMM DD YYYY' }, // Parsing format
+  display: {
+    dateInput: 'ddd MMM DD YYYY', // Display format in input
+    monthYearLabel: 'MMM YYYY', // Format for month/year selector
+    dateA11yLabel: 'LL', // Accessible format for screen readers
+    monthYearA11yLabel: 'MMMM YYYY',
+  },
+};
 @Component({
   selector: 'rise-calendar',
   standalone: true,
@@ -23,13 +35,30 @@ import {MatIcon} from "@angular/material/icon";
     MatNativeDateModule,
     MatFormFieldModule,
     MatInputModule,
-    MatIcon
+    MatIcon,
+    FormsModule,
+    DatePipe
   ],
-  providers: [provideNativeDateAdapter()],
+  providers: [
+    // provideNativeDateAdapter()
+    { provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE] },
+    { provide: MAT_DATE_FORMATS, useValue: CUSTOM_DATE_FORMATS },
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './rise-calendar.component.html',
   styleUrl: './rise-calendar.component.css'
 })
-export class RiseCalendarComponent {
+export class RiseCalendarComponent implements OnInit{
+
+  oSelectedDate: Date = new Date(); // Keep as Date for compatibility
+
+
+  ngOnInit() {
+  }
+  emitDate(event: any): void {
+    console.log(this.oSelectedDate)
+    this.oSelectedDate = event.value; // Update selected date as Date
+  }
+
 
 }
