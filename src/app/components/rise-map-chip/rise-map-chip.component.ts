a51d65d0fb7d0b8740847169294de9e1a74df5f0
jihed-Sh <jihedcr7@gmail.com>
2025-10-20 16:33:15 +0200

    [FIXED] i18n

--- src/app/components/rise-map-chip/rise-map-chip.component.ts
+++ src/app/components/rise-map-chip/rise-map-chip.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {NgClass, NgIf} from '@angular/common';
 
 @Component({
@@ -14,7 +14,7 @@ export class RiseMapChipComponent implements OnInit {
   @Input() m_bIsActive: boolean = false;
   @Input() m_sIcon: string = '';
   @Input() m_sChipType: 'default' | 'toggle' = 'default';
-  @Input() m_bDisabled: boolean=false;
+  @Input() m_bDisabled: boolean = false;
 
 
   ngOnInit() {
@@ -32,16 +32,18 @@ export class RiseMapChipComponent implements OnInit {
       this.m_sIcon = 'light_mode';
     } else if (sIcon.includes('building')) {
       this.m_sIcon = 'domain';
-    }else if (sIcon.includes('rain')) {
+    } else if (sIcon.includes('rain')) {
       this.m_sIcon = 'rainy';
-    }else if (sIcon.includes('impact')) {
+    } else if (sIcon.includes('impact')) {
       this.m_sIcon = 'destruction';
-    }else if (sIcon.includes('more')) {
-      this.m_sIcon = 'chevron_right';
-    }else if (sIcon.includes('less')) {
-      this.m_sIcon = 'chevron_left';
-    }else if (sIcon.includes('active_fire')) {
+    } else if (sIcon.includes('active_fire')) {
       this.m_sIcon = 'mode_heat';
+    } else if (sIcon.includes('lst_map')) {
+      this.m_sIcon = 'device_thermostat';
+    } else if (sIcon.includes('more')) {
+      this.m_sIcon = 'chevron_right';
+    } else if (sIcon.includes('less')) {
+      this.m_sIcon = 'chevron_left';
     } else {
       this.m_sIcon = 'layers';
     }
